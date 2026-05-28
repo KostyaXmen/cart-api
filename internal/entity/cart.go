e7aaa6a64f1c962687347ee43c87fafa38661cd8
KostyaXmen <kivanenko2006@gmail.com>
2026-05-28 14:28:24 +0300

    Add entitis

--- internal/entity/cart.go
+++ internal/entity/cart.go
@@ -1,1 +1,13 @@
 package entity
+
+type Cart struct {
+	ID    int64       `json:"id" db:"id"`
+	Items []CartItem  `json:"items" db:"-"`
+}
+
+type CartPriceCalculation struct {
+	CartID          int64   `json:"cart_id"`
+	TotalPrice      float64 `json:"total_price"`
+	DiscountPercent int     `json:"discount_percent"`
+	FinalPrice      float64 `json:"final_price"`
+}
