6aad6e7b0ddc7cbacb3462e6feeda0dba5d5fc41
KostyaXmen <kivanenko2006@gmail.com>
2026-06-01 16:25:52 +0300

    Add config and testing repo

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"cart-api/internal/config"
+	"cart-api/internal/repository"
+	"cart-api/internal/entity"
+	"fmt"
+	"os"
+	"time"
+	"context"
+)
 
 func main() {
-	fmt.Print("")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Println("Fatal error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(cfg)
+
+	dbConn, err := repository.InitDB(cfg)
+	if err != nil {
+		fmt.Println("Fatal error:", err)
+	}
+	defer dbConn.Close()
+
+	repo := repository.NewRepository(dbConn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cart, err := repo.AddCart(ctx)
+	if err != nil {
+		fmt.Println("Error adding cart in repository:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(cart)
+
+	insertedItem, err := repo.AddCartItem(ctx, 1, entity.AddCartItemRequest{"product2", 10.0})
+
+	if err != nil {
+		fmt.Println("Error adding item in cart:", err)
+	} else {
+		fmt.Println(insertedItem)
+		fmt.Println("List: ")
+		fmt.Println(repo.ViewCart(ctx, 1))
+	}
+	
+
+
 }
