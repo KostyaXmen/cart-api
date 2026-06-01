b1a49e9976e2a32c5423b777a3ec1f7c16f16f9b
KostyaXmen <kivanenko2006@gmail.com>
2026-06-01 16:24:57 +0300

    Add init function

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,7 +1,14 @@
 package repository
 
 import (
+	"cart-api/internal/config"
+	"fmt"
+	"embed"
+
 	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+
+	_ "github.com/lib/pq"
 )
 
 type Repository struct {
@@ -10,4 +17,45 @@ type Repository struct {
 
 func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{db: db}
+}
+
+var embedMigrations embed.FS
+
+func InitDB(cfg config.Config) (*sqlx.DB, error){
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.Mode,
+	)
+
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping() ; err != nil {
+		return nil, err
+	}
+
+	if err := migrateDB(db); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func migrateDB(db *sqlx.DB) error {
+	if err := goose.SetDialect("postgres"); err != nil {
+		return err
+	}
+
+	if err := goose.Up(db.DB, "./db/migrations"); err != nil {
+		return err
+	}
+
+	return nil
 }
