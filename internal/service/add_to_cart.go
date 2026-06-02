7d00708dc32d62b9bc8da597b1e462dd38d2e3b0
KostyaXmen <kivanenko2006@gmail.com>
2026-06-02 11:23:38 +0300

    Remove example of error throwing ? add CalculateDiscount() logic

--- internal/service/add_to_cart.go
+++ internal/service/add_to_cart.go
@@ -9,12 +9,3 @@ import (
 func (s *service) AddToCart(ctx context.Context, cartID int64, item entity.AddCartItemRequest) (entity.CartItem, error) {
 	return s.repo.AddCartItem(ctx, cartID, item)
 }
-
-// func (s *service) AddToCart(ctx context.Context, cartID int64, item entity.AddCartItemRequest) (entity.CartItem, error) {
-// 	cartItem, err := s.repo.AddCartItem(ctx, cartID, item)
-// 	if err != nil {
-// 		return entity.CartItem{}, fmt.Errorf("failed to add item to the cart")
-// 	}
-// 	return cartItem, nil
-// }
-
--- internal/service/calculate_discount.go
+++ internal/service/calculate_discount.go
@@ -5,6 +5,22 @@ import (
 )
 
 func (s *service) CalculateDiscount(ctx context.Context, cartID int64) (float64, error) {
-	// TODO
+	viewedCart, err := s.repo.ViewCart(ctx, cartID)
+	if err != nil {
+		return 0.0, err
+	}
+	var total float64
+	for _, cartItem := range viewedCart.Items {
+		total += cartItem.Price
+	}
+	
+	if total > 5000 {
+		return 0.10, nil
+	}
+
+	if len(viewedCart.Items) > 3 {
+		return 0.5, nil
+	}
+
 	return 0.0, nil 
 }
